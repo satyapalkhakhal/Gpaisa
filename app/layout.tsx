4be07d55fe70bb20aee063970d11380f4238e6b8
satyapalkhakhal <khakhalsatyapal@gmail.com>
2026-01-02 14:55:22 +0530

    analtyics

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,20 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                {/* Google Analytics */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-TV8X50LJB1"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-TV8X50LJB1');
+                        `,
+                    }}
+                />
+            </head>
             <body className="flex flex-col min-h-screen">
                 <Header />
                 <MarketTicker />
